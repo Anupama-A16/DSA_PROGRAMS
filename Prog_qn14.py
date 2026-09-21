f4e82f1540e153fe073a5cc67b6cfd1ddf51efd8
Anupama <anupama.a.bhat16@gmail.com>
2026-09-21 19:03:50 +0530

    Add program to find Fibonacci numbers using recursion

---
+++ Prog_qn14.py
@@ -0,0 +1,10 @@
+# Find the Fibonacci Number using Recursion
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = int(input("Enter a number: "))
+
+for i in range(n):
+    print(fibonacci(i), end=" ")
